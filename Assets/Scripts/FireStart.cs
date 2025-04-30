9ec9ef8806f75ac930dbd957a0851310354148be
finleygrindle <finleygrindle@hotmail.co.uk>
2025-04-30 22:03:46 +0100

    finishing touches

--- Assets/Scripts/FireStart.cs
+++ Assets/Scripts/FireStart.cs
@@ -7,7 +7,12 @@ public class FireStart : MonoBehaviour
     public GameObject BedroomFire;
     public GameObject Disappear;
     public GameObject Player;
+    public GameObject Facts;
     Fader fade;
+
+    public AudioClip gasp;
+    public AudioClip fire;
+    private AudioSource audioSource;
 
     public CanvasGroup canvasGroup;
     public bool fadeout = false;
@@ -18,6 +23,8 @@ public class FireStart : MonoBehaviour
     void Start()
     {
         fade = FindObjectOfType<Fader>();
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -43,7 +50,7 @@ public class FireStart : MonoBehaviour
 
     public IEnumerator ChangeScene()
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(2);
         Player.transform.position = new Vector3(-1.945f, 0.909f, -0.296f);
     }
 
@@ -55,6 +62,9 @@ public class FireStart : MonoBehaviour
             BedroomFire.transform.position = new Vector3(6f, 6f, 4f);
             Disappear.transform.position = new Vector3(30f, 6f, 4f);
             Player.transform.position = new Vector3(0.61f, 0.909f, 11.43f);
+            Facts.transform.position = new Vector3(-16.43114f, -2.5f, -6.887037f);
+            audioSource.PlayOneShot(gasp, 1f);
+            audioSource.PlayOneShot(fire, 1f);
             StartCoroutine(ChangeScene());
         }
     }
